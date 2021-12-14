c73d86c7bf8c57bd1ea389990304a3f6ed992f17
oogioogi <detacl73@gmail.com>
2021-12-14 12:23:19 +0900

    Initial Commit

---
+++ AnotherScrollView/AnotherScrollViewApp.swift
@@ -0,0 +1,17 @@
+//
+//  AnotherScrollViewApp.swift
+//  AnotherScrollView
+//
+//  Created by 이용석 on 2021/12/14.
+//
+
+import SwiftUI
+
+@main
+struct AnotherScrollViewApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
